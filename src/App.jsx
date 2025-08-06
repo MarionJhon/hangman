63fa486940580e38020d8e3fb1b1940ad5e4cc24
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-08-06 21:02:29 +0800

    add header and section game status

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming word safe from assembly!</p>
+      </header>
+      <section className="status">
+        <h1>You win!</h1>
+        <p>Well done🎉</p>
+      </section>
+    </main>
+  )
+}
+
+export default App;
